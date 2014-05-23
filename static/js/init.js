c054b3e0b0cb7de61e76a8856b6b97b4933d3550
Sean Herron <sean@herron.io>
2014-05-23 18:30:18 -0400

    show hide all working now

--- static/js/init.js
+++ static/js/init.js
@@ -2,14 +2,13 @@ $(document).ready(function() {
   $('.below-fold').hide();
   $('.below-fold').after('<button type="button" class="btn btn-link show-below-fold">Show All <i class="fa fa-caret-down"></i></button>');
   
-  $( ".show-below-fold" ).live("click", function() {
-    $(this).prev('.below-fold').show();
-    $(this).replaceWith('<button type="button" class="btn btn-link hide-below-fold">Hide All <i class="fa fa-caret-up"></i></button>')
-  });
-  
-  $( ".hide-below-fold" ).live("click", function() {
+  $(document).on( "click", ".hide-below-fold", function() {
     $(this).prev('.below-fold').hide();
-    $(this).replaceWith('<button type="button" class="btn btn-link show-below-fold">Show All <i class="fa fa-caret-down"></i></button>')
+    $(this).replaceWith('<button type="button" class="btn btn-link show-below-fold">Show All <i class="fa fa-caret-down"></i></button>');
+  });
+
+  $(document).on( "click", ".show-below-fold", function() {
+    $(this).prev('.below-fold').show();
+    $(this).replaceWith('<button type="button" class="btn btn-link hide-below-fold">Hide All <i class="fa fa-caret-up"></i></button>');
   });
 });
-
