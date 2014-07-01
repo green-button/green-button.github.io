650d8cec7096ae61d1cba43365694e98abf14df0
EnergyOS.org Master Admin <github@energyos.org>
2014-07-01 07:03:29 -0600

    Added SmartCities Map
    A map covering Green Button Activities within the Smart City Framework

---
+++ mindmap/Smart Cities Mindmap.mm
@@ -0,0 +1,127 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Smart Cities Mindmap" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1404170826452"><hook NAME="MapStyle">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10"/>
+<node TEXT="Hawaii" POSITION="right" ID="ID_90262307" CREATED="1403388108860" MODIFIED="1403388117328">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="Austin" POSITION="right" ID="ID_645821508" CREATED="1403388118179" MODIFIED="1403388122160">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Chattanooga" POSITION="right" ID="ID_1722259474" CREATED="1403388122739" MODIFIED="1403389423834">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Seattle" POSITION="right" ID="ID_155734284" CREATED="1403389424766" MODIFIED="1403389431610">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Japan" POSITION="left" ID="ID_540848794" CREATED="1403392867363" MODIFIED="1403392873783">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="Korea" POSITION="left" ID="ID_89656377" CREATED="1403392874251" MODIFIED="1403392876535">
+<edge COLOR="#ffff00"/>
+</node>
+<node TEXT="" POSITION="left" ID="ID_98228236" CREATED="1403392877259" MODIFIED="1403392877260">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="Partnership for Sustainable Communities" POSITION="left" ID="ID_475690407" CREATED="1404071956415" MODIFIED="1404072018900">
+<edge COLOR="#00007c"/>
+<node TEXT="Federal Agencies" ID="ID_1983030500" CREATED="1404072031821" MODIFIED="1404072097083">
+<node TEXT="Dept of Housing and Urban Development" ID="ID_1300393024" CREATED="1404072040921" MODIFIED="1404078282195" LINK="http://portal.hud.gov/hudportal/HUD?src=/program_offices/economic_resilience/sustainable_housing_initiative"/>
+<node TEXT="Environmental Protection Agency" ID="ID_441627697" CREATED="1404072071832" MODIFIED="1404078397424" LINK="http://www.epa.gov/smartgrowth/index.htm"/>
+<node TEXT="Dept of Transportation" ID="ID_118971345" CREATED="1404072020217" MODIFIED="1404078332580" LINK="http://www.dot.gov/livability">
+<node TEXT="Jan 29-31 2014 Conference" ID="ID_1308529530" CREATED="1404078401144" MODIFIED="1404078432811" LINK="http://www.newpartners.org/">
+<node TEXT="Postcard from the Edge" ID="ID_1406856729" CREATED="1404078561308" MODIFIED="1404078579452" LINK="http://www.newpartners.org/2015/wp-content/uploads/2014/05/NP15-save-date-postcard.pdf"/>
+</node>
+</node>
+<node TEXT="" ID="ID_1994534621" CREATED="1404072255778" MODIFIED="1404072255778"/>
+</node>
+<node TEXT="Cities" ID="ID_831850138" CREATED="1404072116770" MODIFIED="1404218868868" LINK="http://www.dot.gov/sites/dot.dev/files/docs/DOTEPAHUD.xlsx">
+<node TEXT="Los Angeles" ID="ID_1913054313" CREATED="1404218808451" MODIFIED="1404218813295">
+<node TEXT="Light Rail" ID="ID_302037760" CREATED="1404218903881" MODIFIED="1404218916443" LINK="http://www.sustainablecommunities.gov/pdf/studies/Region9_VermontWilshire.pdf"/>
+</node>
+<node TEXT="Seattle" ID="ID_330987454" CREATED="1404218816243" MODIFIED="1404218820030">
+<node TEXT="South Lake Union Neighborhood" ID="ID_996848793" CREATED="1404218941489" MODIFIED="1404218965333" LINK="http://www.sustainablecommunities.gov/pdf/studies/seattle-south-lake-union.pdf"/>
+</node>
+<node TEXT="Portland" ID="ID_1671825204" CREATED="1404218820770" MODIFIED="1404218823758"/>
+</node>
+<node TEXT="6 Livabilty Principals" ID="ID_862764969" CREATED="1404072142137" MODIFIED="1404072183174">
+<node TEXT="Provide more transportation choices." ID="ID_1777855605" CREATED="1404072218170" MODIFIED="1404072228306"/>
+<node TEXT="Promote equitable, affordable housing." ID="ID_1194379113" CREATED="1404072230011" MODIFIED="1404072235700"/>
+<node TEXT="Enhance economic competitiveness." ID="ID_1590329468" CREATED="1404072237170" MODIFIED="1404072250622"/>
+<node TEXT="Support existing communities.." ID="ID_14204162" CREATED="1404072265105" MODIFIED="1404072270069"/>
+<node TEXT="Coordinate and leverage federal policies and investment." ID="ID_313520995" CREATED="1404072271714" MODIFIED="1404072277493"/>
+<node TEXT="Value communities and neighborhoods" ID="ID_391455890" CREATED="1404072277906" MODIFIED="1404072280133"/>
+</node>
+<node TEXT="Grants" ID="ID_623278605" CREATED="1404219008064" MODIFIED="1404219016746" LINK="http://portal.hud.gov/hudportal/HUD?src=/program_offices/administration/grants/fundsavail">
+<node TEXT="Choice Neighborhood Planning Grants" ID="ID_1156384208" CREATED="1404219018936" MODIFIED="1404219047795">
+<node TEXT="" ID="ID_1537262622" CREATED="1404219032960" MODIFIED="1404219032960"/>
+</node>
+<node TEXT="Comprehensive Neighborhood Counciling Grants" ID="ID_1567051760" CREATED="1404219048760" MODIFIED="1404219067355"/>
+<node TEXT="Family Self-Sufficiency Program" ID="ID_1021863238" CREATED="1404219067912" MODIFIED="1404219083075"/>
+<node TEXT="Healthy Homes and Lead Technical Studies Pre-Application Notice" ID="ID_1919403907" CREATED="1404219077767" MODIFIED="1404219125106"/>
+</node>
+</node>
+<node TEXT="Los Angeles" POSITION="right" ID="ID_1673748883" CREATED="1404072310514" MODIFIED="1404072314820">
+<edge COLOR="#007c00"/>
+</node>
+<node TEXT="EcoDistrict Partner Projects" POSITION="left" ID="ID_828900155" CREATED="1404073527042" MODIFIED="1404073539309">
+<edge COLOR="#7c007c"/>
+<node TEXT="Boston" ID="ID_21713576" CREATED="1404073540178" MODIFIED="1404073542957"/>
+<node TEXT="Los Angeles" ID="ID_1490304771" CREATED="1404073544225" MODIFIED="1404073547213"/>
+<node TEXT="Cambridge" ID="ID_421288595" CREATED="1404073547770" MODIFIED="1404073561477"/>
+<node TEXT="Atlanta" ID="ID_1023096440" CREATED="1404073563377" MODIFIED="1404219153716"/>
+<node TEXT="Seattle" ID="ID_129692425" CREATED="1404219155903" MODIFIED="1404219158132"/>
+<node TEXT="Portland Oregon" ID="ID_1127129589" CREATED="1404219158711" MODIFIED="1404219162220"/>
+<node TEXT="District of Columbia" ID="ID_403488182" CREATED="1404219163640" MODIFIED="1404219170252"/>
+</node>
+</node>
+</map>
